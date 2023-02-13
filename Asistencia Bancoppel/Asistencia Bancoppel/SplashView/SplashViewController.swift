cd483f44982f23a5a6e177ccc6b970794c4be50e
Juan Ramon Rodriguez Gomez <jramon.rodrigue@gmail.com>
2023-02-13 14:29:35 -0600

    Base Project Creation

---
+++ Asistencia Bancoppel/Asistencia Bancoppel/SplashView/SplashViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Asistencia Bancoppel
+//
+//  Created by Juan Ramon on 13/02/23.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
