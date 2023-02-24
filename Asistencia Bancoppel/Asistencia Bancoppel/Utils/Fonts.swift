eb6279efaf54bb35d9daf7faf2ecd186b266245b
Samuel Fuentes <samuel.fn331@gmail.com>
2023-02-24 12:54:56 -0600

    The Fonts file was created and the possible fonts were added

---
+++ Asistencia Bancoppel/Asistencia Bancoppel/Utils/Fonts.swift
@@ -0,0 +1,24 @@
+//
+//  Fonts.swift
+//  Asistencia Bancoppel
+//
+//  Created by Samuel Fuentes Navarrete on 24/02/23.
+//
+
+import UIKit
+
+enum Fonts: String {
+    case SFProBook = "SFProText-Regular"
+    case RobotoRegular = "RobotoRegular"
+    case SFProHeavy = "SFProText-Heavy"
+    case SFProMedium = "SFProText-Medium"
+    case RobotoMedium = "RobotoMedium"
+    case SFProBold = "SFProText-Bold"
+    case RobotoBold = "RobotoBold"
+    case Roboto = "Roboto"
+
+    func of(size: CGFloat) -> UIFont {
+        let font = UIFont(name: rawValue, size: size - 1) ?? UIFont.systemFont(ofSize: size - 1)
+        return font
+    }
+}
