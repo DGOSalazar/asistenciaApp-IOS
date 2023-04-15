c517adb7303ee4fe923e7c70adc554b67089474a
MacBook Pro <ledg1996@gmail.com>
2023-04-14 19:11:00 -0600

    AccountCreationPageViewController updated
    Integration of BottomTitleTextField components

--- Asistencia Bancoppel/Asistencia Bancoppel/Core/AccountCreation/View/Pages/AccountCreationPageViewController.swift
+++ Asistencia Bancoppel/Asistencia Bancoppel/Core/AccountCreation/View/Pages/AccountCreationPageViewController.swift
@@ -21,7 +21,6 @@ internal class AccountCreationPageViewController: UIViewController {
     lazy var vwContainer: UIView = {
        let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        //view.backgroundColor = .orange
         return view
     }()
     
@@ -37,6 +36,47 @@ internal class AccountCreationPageViewController: UIViewController {
         label.minimumScaleFactor = 0.5
         label.textAlignment = .center
         return label
+    }()
+    
+    lazy var vwContent: UIView = {
+       let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    lazy var vwDataContainer: UIView = {
+       let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    lazy var txtfEmail: BottomTitleTextField = {
+       let textField = BottomTitleTextField(title: "Ingresa tu correo Coppel.",
+                                            placeholder: "Correo electrónico",
+                                            delegate: self,
+                                            identifier: "email")
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    lazy var txtfCredential: BottomTitleTextField = {
+       let textField = BottomTitleTextField(title: "Debe contener 8 caracteres.",
+                                            placeholder: "Contraseña",
+                                            delegate: self,
+                                            identifier: "credential",
+                                            isSecure: true)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    lazy var txtfCredentialConfirmation: BottomTitleTextField = {
+       let textField = BottomTitleTextField(title: "Vuelve a escribir la contraseña.",
+                                            placeholder: "Confirmar contraseña",
+                                            delegate: self,
+                                            identifier: "confirmCredential",
+                                            isSecure: true)
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
     }()
     
     
@@ -60,6 +100,12 @@ internal class AccountCreationPageViewController: UIViewController {
         self.view.addSubview(vwContainer)
         
         vwContainer.addSubview(lbTitle)
+        vwContainer.addSubview(vwContent)
+        
+        vwContent.addSubview(vwDataContainer)
+        vwDataContainer.addSubview(txtfEmail)
+        vwDataContainer.addSubview(txtfCredential)
+        vwDataContainer.addSubview(txtfCredentialConfirmation)
         vwContainer.addSubview(btNext)
     }
     
@@ -70,14 +116,38 @@ internal class AccountCreationPageViewController: UIViewController {
             vwContainer.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
             vwContainer.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
             
-            lbTitle.topAnchor.constraint(equalTo: vwContainer.topAnchor),
+            lbTitle.topAnchor.constraint(equalTo: vwContainer.topAnchor, constant: 50),
             lbTitle.leadingAnchor.constraint(equalTo: vwContainer.leadingAnchor),
             lbTitle.trailingAnchor.constraint(equalTo: vwContainer.trailingAnchor),
+            
+            vwContent.topAnchor.constraint(equalTo: lbTitle.bottomAnchor),
+            vwContent.leadingAnchor.constraint(equalTo: vwContainer.leadingAnchor, constant: 20),
+            vwContent.trailingAnchor.constraint(equalTo: vwContainer.trailingAnchor, constant: -20),
+            vwContent.bottomAnchor.constraint(equalTo: btNext.topAnchor),
+            
+            vwDataContainer.topAnchor.constraint(greaterThanOrEqualTo: vwContent.topAnchor),
+            vwDataContainer.centerYAnchor.constraint(equalTo: vwContent.centerYAnchor),
+            vwDataContainer.leadingAnchor.constraint(equalTo: vwContent.leadingAnchor),
+            vwDataContainer.trailingAnchor.constraint(equalTo: vwContent.trailingAnchor),
+            vwDataContainer.bottomAnchor.constraint(lessThanOrEqualTo: vwContent.bottomAnchor),
+            
+            txtfEmail.topAnchor.constraint(equalTo: vwDataContainer.topAnchor),
+            txtfEmail.leadingAnchor.constraint(equalTo: vwDataContainer.leadingAnchor),
+            txtfEmail.trailingAnchor.constraint(equalTo: vwDataContainer.trailingAnchor),
+            
+            txtfCredential.topAnchor.constraint(equalTo: txtfEmail.bottomAnchor, constant: 20),
+            txtfCredential.leadingAnchor.constraint(equalTo: vwDataContainer.leadingAnchor),
+            txtfCredential.trailingAnchor.constraint(equalTo: vwDataContainer.trailingAnchor),
+            
+            txtfCredentialConfirmation.topAnchor.constraint(equalTo: txtfCredential.bottomAnchor, constant: 20),
+            txtfCredentialConfirmation.leadingAnchor.constraint(equalTo: vwDataContainer.leadingAnchor),
+            txtfCredentialConfirmation.trailingAnchor.constraint(equalTo: vwDataContainer.trailingAnchor),
+            txtfCredentialConfirmation.bottomAnchor.constraint(equalTo: vwDataContainer.bottomAnchor),
             
             btNext.heightAnchor.constraint(equalToConstant: 59),
             btNext.widthAnchor.constraint(equalToConstant: 200),
             btNext.centerXAnchor.constraint(equalTo: vwContainer.centerXAnchor),
-            btNext.bottomAnchor.constraint(equalTo: vwContainer.bottomAnchor),
+            btNext.bottomAnchor.constraint(equalTo: vwContainer.bottomAnchor, constant: -20),
         ])
     }
     
@@ -85,4 +155,11 @@ internal class AccountCreationPageViewController: UIViewController {
         delegate?.notifyAccountCreationPageNext()
     }
 }
+
+extension AccountCreationPageViewController: BottomTitleTextFieldDelegate {
+    func bottomTitleTextFieldDidChange(identifier: String, text: String) {
+        print("\(identifier): \(text)")
+    }
+}
+
 
