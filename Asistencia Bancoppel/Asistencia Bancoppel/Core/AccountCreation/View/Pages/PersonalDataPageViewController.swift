29c2f6c120b6780f66a00312512f7a19a98bb813
MacBook Pro <ledg1996@gmail.com>
2023-04-14 19:11:40 -0600

    PersonalDataPageViewController updated
    Integration of LeftTitleTextField components

--- Asistencia Bancoppel/Asistencia Bancoppel/Core/AccountCreation/View/Pages/PersonalDataPageViewController.swift
+++ Asistencia Bancoppel/Asistencia Bancoppel/Core/AccountCreation/View/Pages/PersonalDataPageViewController.swift
@@ -37,6 +37,53 @@ internal class PersonalDataPageViewController: UIViewController {
         return label
     }()
     
+    lazy var vwContent: UIView = {
+       let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    lazy var vwDataContainer: UIView = {
+       let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
+    lazy var txtfName: LeftTitleTextField = {
+       let textField = LeftTitleTextField(title: "Nombre: ",
+                                            placeholder: "",
+                                            delegate: self,
+                                            identifier: "name")
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    lazy var txtfLastnames: LeftTitleTextField = {
+       let textField = LeftTitleTextField(title: "Apellidos:",
+                                            placeholder: "",
+                                            delegate: self,
+                                            identifier: "lastnames")
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    lazy var txtfBirthDate: DatePickerTextField = {
+       let textField = DatePickerTextField(title: "Fecha de nacimiento:",
+                                            placeholder: "dd/mm/aaaa",
+                                            delegate: self,
+                                            identifier: "birthDate")
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
+    
+    lazy var txtfCellphone: LeftTitleTextField = {
+       let textField = LeftTitleTextField(title: "Número celular:",
+                                            placeholder: "",
+                                            delegate: self,
+                                            identifier: "cellphone")
+        textField.translatesAutoresizingMaskIntoConstraints = false
+        return textField
+    }()
     
     lazy var btNext: MainButton = {
         let button = MainButton(title: "Siguiente", enable: true)
@@ -58,6 +105,14 @@ internal class PersonalDataPageViewController: UIViewController {
         self.view.addSubview(vwContainer)
         
         vwContainer.addSubview(lbTitle)
+        vwContainer.addSubview(vwContent)
+        
+        vwContent.addSubview(vwDataContainer)
+        vwDataContainer.addSubview(txtfName)
+        vwDataContainer.addSubview(txtfLastnames)
+        vwDataContainer.addSubview(txtfBirthDate)
+        vwDataContainer.addSubview(txtfCellphone)
+        
         vwContainer.addSubview(btNext)
     }
     
@@ -68,14 +123,43 @@ internal class PersonalDataPageViewController: UIViewController {
             vwContainer.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
             vwContainer.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
             
-            lbTitle.topAnchor.constraint(equalTo: vwContainer.topAnchor),
+            lbTitle.topAnchor.constraint(equalTo: vwContainer.topAnchor, constant: 50),
             lbTitle.leadingAnchor.constraint(equalTo: vwContainer.leadingAnchor),
             lbTitle.trailingAnchor.constraint(equalTo: vwContainer.trailingAnchor),
+            
+            vwContent.topAnchor.constraint(equalTo: lbTitle.bottomAnchor),
+            vwContent.leadingAnchor.constraint(equalTo: vwContainer.leadingAnchor, constant: 20),
+            vwContent.trailingAnchor.constraint(equalTo: vwContainer.trailingAnchor, constant: -20),
+            vwContent.bottomAnchor.constraint(equalTo: btNext.topAnchor),
+            
+            vwDataContainer.topAnchor.constraint(greaterThanOrEqualTo: vwContent.topAnchor),
+            vwDataContainer.centerYAnchor.constraint(equalTo: vwContent.centerYAnchor),
+            vwDataContainer.leadingAnchor.constraint(equalTo: vwContent.leadingAnchor),
+            vwDataContainer.trailingAnchor.constraint(equalTo: vwContent.trailingAnchor),
+            vwDataContainer.bottomAnchor.constraint(lessThanOrEqualTo: vwContent.bottomAnchor),
+            
+            txtfName.topAnchor.constraint(equalTo: vwDataContainer.topAnchor),
+            txtfName.leadingAnchor.constraint(equalTo: vwDataContainer.leadingAnchor),
+            txtfName.trailingAnchor.constraint(equalTo: vwDataContainer.trailingAnchor),
+            
+            txtfLastnames.topAnchor.constraint(equalTo: txtfName.bottomAnchor),
+            txtfLastnames.leadingAnchor.constraint(equalTo: vwDataContainer.leadingAnchor),
+            txtfLastnames.trailingAnchor.constraint(equalTo: vwDataContainer.trailingAnchor),
+            
+            txtfBirthDate.topAnchor.constraint(equalTo: txtfLastnames.bottomAnchor),
+            txtfBirthDate.leadingAnchor.constraint(equalTo: vwDataContainer.leadingAnchor),
+            txtfBirthDate.trailingAnchor.constraint(equalTo: vwDataContainer.trailingAnchor),
+            
+            txtfCellphone.topAnchor.constraint(equalTo: txtfBirthDate.bottomAnchor),
+            txtfCellphone.leadingAnchor.constraint(equalTo: vwDataContainer.leadingAnchor),
+            txtfCellphone.trailingAnchor.constraint(equalTo: vwDataContainer.trailingAnchor),
+            txtfCellphone.bottomAnchor.constraint(equalTo: vwDataContainer.bottomAnchor),
+            
             
             btNext.heightAnchor.constraint(equalToConstant: 59),
             btNext.widthAnchor.constraint(equalToConstant: 200),
             btNext.centerXAnchor.constraint(equalTo: vwContainer.centerXAnchor),
-            btNext.bottomAnchor.constraint(equalTo: vwContainer.bottomAnchor),
+            btNext.bottomAnchor.constraint(equalTo: vwContainer.bottomAnchor, constant: -20),
         ])
     }
     
@@ -83,3 +167,19 @@ internal class PersonalDataPageViewController: UIViewController {
         delegate?.notifyPersonalDataNext()
     }
 }
+
+
+extension PersonalDataPageViewController: LeftTitleTextFieldDelegate {
+    func leftTitleTextFieldDidChange(identifier: String, text: String) {
+        print("\(identifier): \(text)")
+    }
+}
+
+extension PersonalDataPageViewController: DatePickerTextFieldDelegate {
+    func datePickerTextFieldDidChange(identifier: String, text: String) {
+        print("\(identifier): \(text)")
+    }
+}
+
+
+
