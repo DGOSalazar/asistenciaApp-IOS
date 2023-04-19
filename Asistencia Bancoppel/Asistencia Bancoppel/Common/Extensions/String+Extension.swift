105d20af446c0e7d151bde4250bfa26eea54e178
LuisDiaz <ledg1996@gmail.com>
2023-04-19 13:53:25 -0600

    Remove accent marks func added to string extension

--- Asistencia Bancoppel/Asistencia Bancoppel/Common/Extensions/String+Extension.swift
+++ Asistencia Bancoppel/Asistencia Bancoppel/Common/Extensions/String+Extension.swift
@@ -22,4 +22,10 @@ extension String {
             return false
         }
     }
+    
+    func removeAccentMarks() -> String {
+        let cleanLowercased = (self.replacingOccurrences(of: "á", with: "a").replacingOccurrences(of: "é", with: "e").replacingOccurrences(of: "í", with: "i").replacingOccurrences(of: "ó", with: "o").replacingOccurrences(of: "ú", with: "u"))
+        let cleanUppercased = cleanLowercased.replacingOccurrences(of: "Á", with: "A").replacingOccurrences(of: "É", with: "E").replacingOccurrences(of: "Í", with: "I").replacingOccurrences(of: "Ó", with: "O").replacingOccurrences(of: "Ú", with: "U")
+        return cleanUppercased
+    }
 }
