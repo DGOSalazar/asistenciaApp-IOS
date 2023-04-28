795a138bfad084790e992cf6181e09ea0563084b
LuisDiaz <ledg1996@gmail.com>
2023-04-27 18:46:48 -0600

    DayAttendanceModel and attendanceMark_Icon added

---
+++ Asistencia Bancoppel/Asistencia Bancoppel/Modules/AccountHome/Model/DayAttendanceModel.swift
@@ -0,0 +1,14 @@
+//
+//  DayAttendance.swift
+//  Asistencia Bancoppel
+//
+//  Created by MacBook Pro on 26/04/23.
+//
+
+import Foundation
+
+
+struct DayAttendanceModel: Codable {
+    var currentDay: String?
+    var email: [String]?
+}
